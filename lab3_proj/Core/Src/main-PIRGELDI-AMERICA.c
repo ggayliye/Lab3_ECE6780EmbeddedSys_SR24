074b452b92f7e7b6c101faf33caa2c80bd983a19
ggayliye <122452379+ggayliye@users.noreply.github.com>
2024-02-21 00:49:05 -0700

    The whole project.

---
+++ lab3_proj/Core/Src/main-PIRGELDI-AMERICA.c
@@ -0,0 +1,576 @@
+		/* USER CODE BEGIN Header */
+		/**
+		******************************************************************************
+		* @file           : main.c
+		* @brief          : Main program body
+		******************************************************************************
+		* @attention
+		*
+		* Copyright (c) 2024 STMicroelectronics.
+		* All rights reserved.
+		*
+		* This software is licensed under terms that can be found in the LICENSE file
+		* in the root directory of this software component.
+		* If no LICENSE file comes with this software, it is provided AS-IS.
+		*
+		******************************************************************************
+		*/
+		/* USER CODE END Header */
+		/* Includes ------------------------------------------------------------------*/
+		#include "main.h"
+
+		volatile short button_count=0;
+		volatile short led_lit_count=0;
+		volatile short timer=0;
+		/* Private includes ----------------------------------------------------------*/
+		/* USER CODE BEGIN Includes */
+
+		/* USER CODE END Includes */
+
+		/* Private typedef -----------------------------------------------------------*/
+		/* USER CODE BEGIN PTD */
+
+		/* USER CODE END PTD */
+
+		/* Private define ------------------------------------------------------------*/
+		/* USER CODE BEGIN PD */
+
+		/* USER CODE END PD */
+
+		/* Private macro -------------------------------------------------------------*/
+		/* USER CODE BEGIN PM */
+
+		/* USER CODE END PM */
+
+		/* Private variables ---------------------------------------------------------*/
+
+		/* USER CODE BEGIN PV */
+
+		/* USER CODE END PV */
+
+		/* Private function prototypes -----------------------------------------------*/
+		void SystemClock_Config(void);
+		/* USER CODE BEGIN PFP */
+
+		/* USER CODE END PFP */
+
+		/* Private user code ---------------------------------------------------------*/
+		/* USER CODE BEGIN 0 */
+
+		/* USER CODE END 0 */
+
+		/**
+		* @brief  The application entry point.
+		* @retval int
+		*/
+
+
+
+		// Helper function for toggle handler:
+		void ToggleFunction(int button_count) {
+		// TIM1_BRK_UP_TRG_COM_IRQHandler();
+		if (button_count == 1){
+			
+				if (led_lit_count == 0){
+
+						//GPIOC->BSRR  |= 0x100 ;  // ORANGE ON! GPIOC->BSRR  |= 0x100 | 0x2FF ; 
+
+						GPIOC->ODR &= 0x2FF; //Green On
+						GPIOC->ODR |= 0x200;// orange off.(GPIOC->BSRR&0x2FF) | 0x200; //make GREEN back ON. //GPIOC->ODR |= (1<<9);
+
+				}
+				led_lit_count++;
+				if (led_lit_count > 1){
+
+						GPIOC->ODR &= 0x1FF;// make GREEN OFF.  
+						GPIOC->ODR |= 0x100; //orange ON
+
+						led_lit_count = 0;
+				}
+
+				button_count = 0;
+		}
+		}
+
+
+
+		// Toggle between the green (PC8) and orange (PC9) LEDs in the interrupt handler
+
+		void TIM2_IRQHandler(void) {
+
+
+			 //button_count = 1;
+			 ToggleFunction(1);
+
+		//Don’t forget to clear the pending flag for the update interrupt in the status register:
+
+		//	TIM2->SR |= 0x1;  //Bit 0 UIF: Update interrupt flag	 
+		TIM2->SR &= ~TIM_SR_UIF;
+
+
+		//	
+		//	 timer++;
+		//	
+		//	//if(timer == 6000){
+		//if(led_lit_count == 0)
+		//{
+
+		//            //GPIOC->BSRR  |= 0x100 ;  // ORANGE ON! GPIOC->BSRR  |= 0x100 | 0x2FF ; 
+
+		//            GPIOC->ODR &= 0x2FF; //Green On
+		//            GPIOC->ODR |= 0x200;// orange off.(GPIOC->BSRR&0x2FF) | 0x200; //make GREEN back ON. //GPIOC->ODR |= (1<<9);
+
+		//        }
+		//        led_lit_count++;
+		//        if (led_lit_count > 1){
+
+		//            GPIOC->ODR &= 0x1FF;// make GREEN OFF.  
+		//            GPIOC->ODR |= 0x100; //orange ON
+
+		//            led_lit_count = 0;
+		//				}
+		//timer = 0;
+		////}
+
+
+
+
+
+		}	
+
+
+
+
+		int main(void)
+		{
+		/* USER CODE BEGIN 1 */
+
+		/* USER CODE END 1 */
+
+		/* MCU Configuration--------------------------------------------------------*/
+
+		/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+		HAL_Init();
+
+		/* USER CODE BEGIN Init */
+
+		/* USER CODE END Init */
+
+		/* Configure the system clock */
+		SystemClock_Config();
+
+		/* USER CODE BEGIN SysInit */
+
+		/* USER CODE END SysInit */
+
+		/* Initialize all configured peripherals */
+		/* USER CODE BEGIN 2 */
+
+
+
+		// START: Exersize 3.1 — Using Timer Interrupts:
+
+		//Enable the timer 2 peripheral (TIM2) in the RCC
+		RCC->APB1ENR |= 0x1;  //Bit 0 TIM2EN: TIM2 timer clock enable
+
+		//	RCC->APB2ENR |=0x800; 
+
+		//Configure the timer to trigger an update event:
+		//The default processor frequency of the STM32F072 is 8 MHz
+		//a 16-bit timer can only count up to 65535. 
+		//If your target ARR is outside of that range, 
+		//you’ll need to adjust the prescaler (change units) to scale the ARR appropriately.
+
+		//PSC = fCLK/(ARR * fTARGET) -1.
+		// fCLK=8MHz; fTARGET = 4Hz. 1/4 =0.25sec=250 ms.
+		//Set ARR = 250 (only 8 bits); PSC = 8MHz/(250 * 4Hz) -1 =7999 (occupies 13 bit only out of 16 bit).
+		TIM2->PSC = 7999; //0x1F3F; //7999
+		TIM2->ARR = 250;//0xFA;   //250
+
+		//Configure the timer to generate an interrupt on the UEV event (Update Event).
+		//Use the DMA/Interrupt Enable Register (DIER) to enable the Update Interrupt:
+		TIM2->DIER |= 0x1; //Bit 0 UIE: Update interrupt enable
+		// TIM2->DIER |= TIM_DIER_UIE; //
+		//egr, rcr, CR1 (have that) feedback
+		//Configure and enable/start the timer
+		//Although the RCC enabled the timer’s clock source, 
+		//the timer has its own enable/start bit in the control registers
+     
+		 TIM2->CR1 |= 0x1; //Bit 0 CEN: Counter enable
+		//TIM2->CR1= ((TIM2->CR1 &	0xFF8E) | 0x1);
+		//Bits 6:5 CMS[1:0]: Center-aligned mode selection
+		//Bit 4 DIR: Direction
+		//Bit 0 CEN: Counter enable
+		//Set up the timer’s interrupt handler, and enable in the NVIC.
+
+		NVIC_EnableIRQ(TIM2_IRQn); // In the stm32f072xb.h file.
+
+		NVIC_SetPriority(TIM2_IRQn,1);// Set the priority for the interrupt to 1 (high-priority). 
+
+
+		// Toggle between the green (PC8) and orange (PC9) LEDs in the interrupt handler
+
+		//Initializing all of the LED pins
+
+		RCC->AHBENR |= 0x80000; //For C port (LED ports), "Bit 19 IOPCEN: I/O port C clock enable". 19th bit position. 
+		// We know 19th bit from data sheet RM0091 on page 122.
+		// PC6 is connected to RED LED. 
+		// PC7 is connected to BLUE LED.
+		// PC8 is connected to ORANGE LED.
+		// PC9 is connected to GREEN LED.
+
+
+		//Enable ALL LEDs for "General purpose output mode, "01" as bits"
+		//	GPIOC->MODER |= (85<<12); // This means: Move Right to left as 12 digits (the digits are in bits).
+
+		//then start adjusting from right to left converting 85 to binary and applying OR "|".
+		// 85 in binary will be 01 01 01 01.
+				
+		GPIOC->MODER &= 0xFFF0FFFF; //reset pins 8 through 9.
+		GPIOC->MODER |= 0x50000; //set PC8 and PC9 to "01:General purpose output mode"
+
+		//	GPIOC->MODER |= 0x5A000; //set PC8 and PC9 to "01:General purpose output mode"
+		// PC6 and PC7, set 10: Alternate function mode
+
+		GPIOC->OTYPER &= 0x0; //no OR (|) . GPIO port output type register
+
+		GPIOC->OSPEEDR &= 0x0; //GPIO port output speed register to Low speed
+
+		GPIOC->PUPDR &= 0x0; //GPIO port pull-up/pull-down register
+
+		//Don’t forget to clear the pending flag for the update interrupt in the status register:
+
+		//TIM1->SR |= 0x1;  //Bit 0 UIF: Update interrupt flag
+
+
+		//RCC->AHBENR |= 0x400000;1: I/O port F clock enabled
+		//  RCC->AHBENR |= 0x40; // 1: CRC clock enabled
+
+		 
+		//	GPIOC->ODR |= 0x100; //Set the orange ON;//Set the orange ON, PC8.
+		// END: Exersize 3.1 — Using Timer Interrupts:
+        
+		
+				//Bit 0 UG: Update generation
+				//1: Re-initialize the counter and generates an update of the registers. Note that the prescaler
+				//counter is cleared too (anyway the prescaler ratio is not affected). The counter is cleared if
+				//the center-aligned mode is selected or if DIR=0 (upcounting), else it takes the auto-reload
+				//value (TIMx_ARR) if DIR=1 (downcounting).
+				
+				
+				
+				
+		// START: Exersize 3.2 — Configuring Timer Channels to PWM Mode.
+
+		//1. Enable the timer 3 peripheral (TIM3) in the RCC.
+		RCC->APB1ENR |= 0x2;  //Bit 1 TIM3EN: TIM3 timer clock enable
+		// RCC->APB1ENR |=0x2;
+		//2. The timer’s update period determines the period of the PWM signal; 
+		//configure the timer to a UEV period related to 800 Hz (T = 1/f).
+		// T = 0.00125
+		//RCC->APB1RSTR |= 0x2;Reset TIM3
+		//Configure the timer to trigger an update event:
+		//The default processor frequency of the STM32F072 is 8 MHz
+		//a 16-bit timer can only count up to 65535. 
+		//If your target ARR is outside of that range, 
+		//you’ll need to adjust the prescaler (change units) to scale the ARR appropriately.
+		 //TIM3->CNT =2;
+		//PSC = fCLK/(ARR * fTARGET) -1.
+		// fCLK=8MHz; fTARGET = 800Hz. 1/800 =0.00125sec= 1.25 ms.
+		//Set ARR = 1.25 (only 8 bits); PSC = 8MHz/(1.25 * 800Hz) -1 =7999 (occupies 13 bit only out of 16 bit).
+		TIM3->PSC = 999; //0x1F3F; //79
+		TIM3->ARR = 10;//0xFA; (1.25 originally)  //800Hz
+    
+		
+		//	TIM3->DIER |= 0x1; //Bit 0 UIE: Update interrupt enable
+		//TIM3->ARR = (125/100);//0xFA; (1.25 originally)  //800Hz
+		// 3. Use the Capture/Compare Mode Register 1 (CCMR1) 
+		// register to configure the output channels to PWM mode
+
+		 //TIM3->CR1 |= 0x1; //Bit 0 CEN: Counter enable
+		 // TIM3->CR1= ((TIM3->CR1 &	0xFF8E) | 0x1);
+		// TIM3->CR2|= 0x88; 
+		//(a) The CCMR1 register configures channels 1 & 2, 
+		//and the CCMR2 register for channels 3 & 4. 
+		//(b) Examine the bit definitions for the CC1S[1:0] 
+		//and CC2S[1:0] bit fields; ensure that you set the channels to output.
+		 
+		 
+		TIM3->CCER &=0xFFEE;
+		TIM3->CCMR1 &=0xFCFC; // Bits 1:0 CC1S: Capture/Compare 1 selection. Bits 9:8 CC2S: Capture/compare 2 selection
+		//00: CC1 & CC2 channel is configured as output.
+		// Note: CC2S bits are writable only when the channel is OFF (CC2E = 0 in TIMx_CCER).
+		//Therefore, I added the code above.
+
+
+		//(c) Examine the bit definitions for the OC1M[2:0] bit field; set output channel 1 to PWM Mode 2.		
+		// Bits 6:4 OC1M: Output compare 1 mode 
+		// 111: PWM mode 2 - In upcounting, channel 1 is inactive as long as TIMx_CNT<TIMx_CCR1 
+		//else active. In downcounting, channel 1 is active as long as TIMx_CNT>TIMx_CCR1 else inactive.
+
+		//	TIM3->CCMR1 |=0x70; // Binary Val:0000000001110000. 
+
+
+		// (d) Use the OC2M[2:0] bit field to set channel 2 to PWM Mode 1.
+		// TIM3->CCMR1 = (0x7000 & 0xEFFF); //Bits 14:12 OC2M[2:0]:
+
+		//	TIM3->CCMR1 = ((TIM3->CCMR1 & 0x8FFF) | 0x6000);
+		// c) & d):
+		TIM3->CCMR1 |=0x70;
+		TIM3->CCMR1 |=0x7000;
+		// or activate both at the same time: TIM3->CCMR1 |=0x7070;
+		//	TIM3->CCMR1 &=0xEFFF;
+
+
+		// (e) Enable the output compare preload for both channels.
+
+		// TIM3->CCMR1 |=0x8; // Bit 3 OC1PE: Output compare 1 preload enable
+		//	TIM3->CCMR1 |=0x800; // Bit 11 OC2PE: Output compare 2 preload enable
+
+		 TIM3->CCMR1 |=0x808;
+		 
+		//1: Preload register on TIMx_CCR1 enabled.
+
+		//4. Set the output enable bits for channels 1 & 2 in the CCER register.
+			
+		//TIM3->CCER |=0x1;   //Bit 0 CC1E: Capture/Compare 1 output enable.
+		//TIM3->CCER |=0x10;  // Bit 4 CC2E: Capture/Compare 2 output enable.
+
+		 TIM3->CCER |=0x11; 
+		 
+		//5. Set the capture/compare registers (CCRx) for both channels to 20% of your ARR value.
+		TIM3->CCR1 =2; //ARR=5 x 0.2 = 1. or ARR=250 x 0.2 = 50.
+		TIM3->CCR2 =2; //ARR=5 x 0.2 = 1.
+
+		//TIM3->CCR2 =(25/100); //ARR=5 x 0.2 = 1.
+
+		 // NVIC_EnableIRQ(TIM3_IRQn); // In the stm32f072xb.h file.
+
+		// 3.3 — Configuring Pin Alternate Functions
+		//1. Look up the alternate functions of the red (PC6) and blue (PC7) LEDs
+
+		// Look at the table in "STM32F072x8 STM32F072xB" datasheet, page 46.
+		// AF0=> PC6: TIM3_CH1; PC7: TIM3_CH2
+
+
+		GPIOC->MODER &= 0xFFFF0FFF; // make PC7 and PC6 zero. Binary: 11111111111111110000111111111111
+		 //  HAL_Delay(460);                                                                                                                                                                                                                                                                	
+			//TIM3->SR |= 0x1;
+		//	TIM3->SR &= ~TIM_SR_UIF;
+		//TIM3->CR1=0;
+		//GPIOC->MODER |= 0x5000;
+		GPIOC->MODER |= 0xA000; //10: Alternate function mode. Binary: 00000000000000001010000000000000
+		//GPIOC->MODER |= 0xA000;
+		//made moder6 and moder7 "0" first and multiply that 
+		//by appropriate binary so we can activatered (PC6)& (PC7).
+		// PC6 and PC7, set 10: Alternate function mode
+		//[only red is on right now since "3.3 — Configuring Pin Alternate Functions"]
+
+
+
+
+		//		GPIOC->OTYPER &= 0x0; //no OR (|) . GPIO port output type register
+
+		//		GPIOC->OSPEEDR &= 0x0; //GPIO port output speed register to Low speed
+
+		//		GPIOC->PUPDR &= 0x0; //GPIO port pull-up/pull-down register
+
+		//Don’t forget to clear the pending flag for the update interrupt in the status register:
+
+		//TIM1->SR |= 0x1;  //Bit 0 UIF: Update interrupt flag
+
+
+		//	GPIOC->ODR |= 0x100; //Set the orange ON
+
+		//TIM3->BDTR |= TIM_BDTR_MOE; /* (6) */
+		//TIM3->CR1 |= TIM_CR1_CEN; /* (7) */
+		//TIM3->CR1 |= TIM_CR1_CEN; /* (7) */
+		//TIM3->EGR |= TIM_EGR_UG; /* (8) */
+
+		//      GPIOC->ODR &= 0xFF3F; //reset red and blue
+		//			 
+		//			GPIOC->BSRR |= 0xC0; //reset red and blue
+	// 
+
+		//TIM2 and TIM3 control register 2 (TIM2_CR2 and TIM3_CR2)
+		//0: The TIMx_CH1 pin is connected to TI1 input . 
+		//1: The TIMx_CH1, CH2 and CH3 pins are connected to the TI1 input (XOR combination)
+		// TI1S is 7th bit.
+		//	__IO uint32_t AFR[2]
+
+		//From the 
+		//	 GPIOC->AFR[0] |= (0x0 << GPIO_AFRL_AFRL6_Pos) |(0x0 << GPIO_AFRL_AFRL7_Pos); 
+		// GPIOC->ODR |= 0x80; //blue is on, PC7
+     //TIM3->CNT=0;
+		GPIOC->AFR[0] &= 0xFFFFFF ; //Binary:00000000111111111111111111111111
+		//TIM3->CR1=1;
+			 //AFR[0] means AFRL because there is L and H. H is AFR[1].
+
+		// GPIOC->AFR[0] |= (0x0 << GPIO_AFRL_AFRL6_Pos) ; 
+		// GPIOC->AFR[1] |= (0x0 << GPIO_AFRL_AFRL7_Pos); 
+		 
+		// TIM3->CCMR1 |=0x8080;
+		//GPIOC->AFR[0] |= (0x0 << GPIO_AFRL_AFRL6_Pos) | (0x0 << GPIO_AFRL_AFRL7_Pos); 
+		// IN binary:00000000111111111111111111111111 for AF0 and PC6& PC7.
+		// GPIOC->AFR[0] &= 0xFFFFFF; // IN binary:00000000111111111111111111111111 for AF0 and PC6& PC7. 
+		//Located in  AFRL not in AFRH
+
+		//	3.4 — Measuring PWM Output.
+
+		
+			
+			
+			
+			
+			
+			
+			
+
+			
+			
+			
+						
+				// TIM3->EGR |= 0x6;
+					TIM2->EGR |= 0x1; //reset tim2
+				//	TIM3->EGR |= 0x1; //reset tim3
+			
+
+			
+			
+			
+			
+			
+			
+			
+			
+ //uint16_t CurrentTimerVal = 0;
+
+		/* USER CODE END 2 */
+
+		/* Infinite loop */
+		/* USER CODE BEGIN WHILE */
+		// int i;
+		while (1)
+		{
+			
+//			        while(TIM3->CCR2 <10)
+//        {
+//            TIM3->CCR2 = TIM3->CCR2;
+//            TIM3->CCR2 += 1;
+//            HAL_Delay(1);
+//        }
+//        while(TIM3->CCR2 > 2)
+//        {
+//            TIM3->CCR2 = TIM3->CCR2;
+//            TIM3->CCR2 -= 1;
+//            HAL_Delay(1);
+//        }
+			
+			
+		/* USER CODE END WHILE */
+
+		//Toggle the red LED (PC6) with a moderately-slow delay (400-600ms) in the infinite loop
+		// GPIOC->BSRR |= (1<<9);
+		//		
+		//			
+		//		HAL_Delay(250);
+		//			
+		 // TIM1_BRK_UP_TRG_COM_IRQHandler(); 	
+					
+		//			for (i = 0; i < 2500000; i++) {
+		//					// delay of one and half second
+		//				if(i==2499998) {
+		//				//
+		//						ToggleFunction();
+		//				}
+		//			}
+		//		HAL_Delay(350);
+		//		ToggleFunction(1);
+		//	TIM1_BRK_UP_TRG_COM_IRQHandler();
+
+		//			
+    if (GPIOC->ODR | (0x40)) // if red is on, red LED (PC6)
+    {
+        GPIOC->ODR &= 0x3BF;  // make it off
+        HAL_Delay(460); //wait 460 ms so that program can catch the off signal
+    }
+
+    GPIOC->ODR |= (0x40); //make it on back 
+		
+	HAL_Delay(460);
+
+		/* USER CODE BEGIN 3 */
+		}
+		/* USER CODE END 3 */
+		}
+
+		/**
+		* @brief System Clock Configuration
+		* @retval None
+		*/
+		void SystemClock_Config(void)
+		{
+		RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+		RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+		/** Initializes the RCC Oscillators according to the specified parameters
+		* in the RCC_OscInitTypeDef structure.
+		*/
+		RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+		RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+		RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+		RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+		if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+		{
+		Error_Handler();
+		}
+
+		/** Initializes the CPU, AHB and APB buses clocks
+		*/
+		RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+															|RCC_CLOCKTYPE_PCLK1;
+		RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+		RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+		if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+		{
+		Error_Handler();
+		}
+		}
+
+		/* USER CODE BEGIN 4 */
+
+		/* USER CODE END 4 */
+
+		/**
+		* @brief  This function is executed in case of error occurrence.
+		* @retval None
+		*/
+		void Error_Handler(void)
+		{
+		/* USER CODE BEGIN Error_Handler_Debug */
+		/* User can add his own implementation to report the HAL error return state */
+		__disable_irq();
+		while (1)
+		{
+		}
+		/* USER CODE END Error_Handler_Debug */
+		}
+
+		#ifdef  USE_FULL_ASSERT
+		/**
+		* @brief  Reports the name of the source file and the source line number
+		*         where the assert_param error has occurred.
+		* @param  file: pointer to the source file name
+		* @param  line: assert_param error line source number
+		* @retval None
+		*/
+		void assert_failed(uint8_t *file, uint32_t line)
+		{
+		/* USER CODE BEGIN 6 */
+		/* User can add his own implementation to report the file name and line number,
+		 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+		/* USER CODE END 6 */
+		}
+		#endif /* USE_FULL_ASSERT */
---
+++ lab3_proj/MDK-ARM/lab3_proj/stm32f0xx_hal_i2c.d
@@ -0,0 +1,31 @@
+lab3_proj/stm32f0xx_hal_i2c.o: \
+  ..\Drivers\STM32F0xx_HAL_Driver\Src\stm32f0xx_hal_i2c.c \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal.h \
+  ..\Core\Inc\stm32f0xx_hal_conf.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f072xb.h \
+  ..\Drivers\CMSIS\Include\core_cm0.h \
+  D:\.embArmK\ARM\ARMCLANG\include\stdint.h \
+  D:\OneDrive\ -\ University\ of\ Utah\.UofU\.Masters\2.Spring2024\1.ECE6780\ EmbeddedSysDesign\.LABs\lab3\lab3_proj\Drivers\CMSIS\Include\cmsis_version.h \
+  D:\OneDrive\ -\ University\ of\ Utah\.UofU\.Masters\2.Spring2024\1.ECE6780\ EmbeddedSysDesign\.LABs\lab3\lab3_proj\Drivers\CMSIS\Include\cmsis_compiler.h \
+  D:\OneDrive\ -\ University\ of\ Utah\.UofU\.Masters\2.Spring2024\1.ECE6780\ EmbeddedSysDesign\.LABs\lab3\lab3_proj\Drivers\CMSIS\Include\cmsis_armclang.h \
+  D:\.embArmK\ARM\ARMCLANG\include\arm_compat.h \
+  D:\.embArmK\ARM\ARMCLANG\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  D:\.embArmK\ARM\ARMCLANG\include\stddef.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_exti.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_cortex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr_ex.h
